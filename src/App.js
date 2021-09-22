e0365ddd90cf69aacc5bd78ec76c7e7e5eb4a8c2
federicorizzato75@gmail.com <fede75@gmail.com>
2021-09-22 20:16:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import countries from 'i18n-iso-countries';
+
+countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
 
 function App() {
+  // Stato
+  const [apiData, setApiData] = useState({});
+  const [getState, setGetState] = useState('tamilnadu');
+  const [state, setState] = useState('tamilnadu');
+
+  // API KEY AND URL
+  const apiKey = "71c406cd0696048af1069afda7277927";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+
+  // Side effect
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+  }, [apiUrl]);
+
+  const inputHandler = (event) => {
+    setGetState(event.target.value);
+  };
+
+  const submitHandler = () => {
+    setState(getState);
+  };
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <div className="container">
+       
+          
+          
+            <input
+              type="text"
+              id="location-name"
+              class="form-control"
+              onChange={inputHandler}
+              value={getState}
+            />
+         
+          <button className="btn btn-primary mt-2" onClick={submitHandler}>
+            Search
+          </button>
+       
+
+
+          {apiData.main ? (
+           
+              <p className="h2">
+                {apiData.main.temp- 273.15}&deg; C
+              </p>
+
+    
+          ) : (
+            <h1>Loading</h1>
+          )}      
+      </div>
     </div>
   );
 }
